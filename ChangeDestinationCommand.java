2700359e5e8d1e6a71e8f1bd9f5c0cfd596f7a60
Aman Nurgozhiyev <nurgozhiyevaman@gmail.com>
2024-10-16 21:40:11 +0500

    Create ChangeDestinationCommand.java

---
+++ ChangeDestinationCommand.java
@@ -0,0 +1,14 @@
+public class ChangeDestinationCommand implements Command {
+    private TaxiOrder order;
+    private String newDestination;
+
+    public ChangeDestinationCommand(TaxiOrder order, String newDestination) {
+        this.order = order;
+        this.newDestination = newDestination;
+    }
+
+    @Override
+    public void execute() {
+        order.changeDestination(newDestination);
+    }
+}
